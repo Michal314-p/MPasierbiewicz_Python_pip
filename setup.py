aa3c2a3ab081afdefd3a0a3e51cb01b0a118ee61
Michal314-p <59016764+Michal314-p@users.noreply.github.com>
2020-06-03 21:04:39 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Aplikacja Azon",
+    version="0.2",
+    author="Michał Pasierbiewicz",
+    author_email="226001@student.pwr.edu.pl",
+    description="Aplikacja do używania AZONu",
+    license='MIT',
+    url='https://github.com/Michal314-p/MPasierbiewicz_Python_pip.git',
+    packages=find_packages(exclude=['tests']),
+    python_requires='>=3.6',
+    long_description=open('README.md').read(),
+    zip_safe=False
+)
